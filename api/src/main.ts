b739f331f6949d14518846da381704a9bab94367
Chris Pinkney <hey@chrispinkney.com>
2023-04-26 15:44:29 -0400

    added pong route and (dev) env file

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('main.ts');
+  const app = await NestFactory.create(AppModule, { cors: true });
+  
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  
+  await app.listen(port);
+  logger.log(`App started on port ${port}`);
 }
 bootstrap();
